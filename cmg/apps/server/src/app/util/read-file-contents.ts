ad4f23e96c96fe2c049a79b6144dbe9d8058aad4
Brandon Pliska <brandon.pliska@gmail.com>
2023-08-31 16:08:33 -0700

    fix concat to writable

--- cmg/apps/server/src/app/util/read-file-contents.ts
+++ cmg/apps/server/src/app/util/read-file-contents.ts
@@ -1,22 +1,41 @@
 import * as fs from 'fs'
 import * as readline from 'readline'
+import { Writable } from 'stream'
 
 export function readFileContents(filePath: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    let contents = ''
+    const contents: string[] = []
     const input: fs.ReadStream = fs.createReadStream(filePath, 'utf-8')
     const reader: readline.Interface = readline.createInterface({ input })
 
-    reader.on('line', (line) => {
-      contents += `${line}\n`
+    const writer = new Writable({
+      write(chunk, _, callback) {
+        contents.push(chunk.toString())
+        callback()
+      },
     })
 
-    reader.on('close', async () => {
-      if (contents.length > 0) {
-        resolve(contents)
+    reader.on('line', (line) => {
+      writer.write(`${line}\n`)
+    })
+
+    reader.on('close', () => {
+      writer.end()
+    })
+
+    writer.on('finish', () => {
+      const completeContents = contents.join('')
+      if (completeContents.length > 0) {
+        resolve(completeContents)
       } else {
         reject('File contents are empty')
       }
     })
+
+    reader.on('error', (error) => {
+      reject(error)
+    })
+
+    input.pipe(writer)
   })
 }
